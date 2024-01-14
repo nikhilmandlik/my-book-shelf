b7caaebd8111dc6c86b96fb848c0813def1cced2
Nikhil Mandlik <nikhiljmandlik@gmail.com>
2024-01-14 03:17:13 -0800

    added React Hook Form

--- src/BookForm.tsx
+++ src/BookForm.tsx
@@ -1,4 +1,10 @@
 import { useState } from 'react';
+import {
+  useForm,
+  SubmitHandler,
+  useFieldArray,
+  FieldArrayWithId,
+} from 'react-hook-form';
 import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 
 interface Highlights {
@@ -22,47 +28,58 @@ export interface BookFormProps {
   highlights: Highlights[];
 }
 
+interface HighlightsProps {
+  field: FieldArrayWithId<BookFormProps, 'highlights', 'id'>;
+  index: number;
+}
+
 const defaultHighlightRow = {
   pageNumber: '',
   pageText: '',
 };
 
 function BookForm(props: BookFormProps) {
-  const [highlightRows, setHighlightRows] = useState([]);
+  const { control, register, handleSubmit } = useForm({
+    mode: 'onTouched',
+    reValidateMode: 'onSubmit',
+    defaultValues: props,
+  });
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'highlights',
+  });
 
-  function addBook() {
-    console.log('addbook');
+  function addBook(data) {
+    console.log('addbook', data);
   }
 
-  function removeHightlight(e) {
-    const id = e.target.closest('button').id;
-    const newHighlightRows = highlightRows.filter(
-      (_highlightRow, index) => index.toString() !== id
-    );
+  function HighlightRow(highlightsProps: HighlightsProps) {
+    const { field, index } = highlightsProps;
 
-    console.log(newHighlightRows);
-
-    setHighlightRows(newHighlightRows);
-  }
-
-  function HighlightRow({ id }) {
     return (
-      <Form.Group as={Row} className="mb-4" controlId={id}>
+      <Form.Group as={Row} className="mb-4" controlId={field.id}>
         <Form.Label column sm={2}>
           Page highlight
         </Form.Label>
         <Col sm={10} className="d-flex align-items-start ">
           <div className="flex-grow-1 pe-2">
             <div className="mb-2 d-inline-flex justify-content-between">
-              <Form.Control type="number" placeholder="Page number" />
+              <Form.Control
+                type="text"
+                placeholder="Page number"
+                {...(register(`highlights.${index}.pageNumber`),
+                { required: true })}
+              />
             </div>
             <Form.Control
               as="textarea"
               rows={2}
               placeholder="Enter highlight text here"
+              {...(register(`highlights.${index}.pageText`),
+              { required: true })}
             />
           </div>
-          <Button onClick={removeHightlight} id={id}>
+          <Button onClick={() => remove(index)} id={field.id}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="16"
@@ -70,7 +87,7 @@ function BookForm(props: BookFormProps) {
               fill="currentColor"
               className="bi bi-trash"
               viewBox="0 0 16 16"
-              id={id}
+              id={field.id}
             >
               <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
               <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
@@ -84,16 +101,20 @@ function BookForm(props: BookFormProps) {
   return (
     <Container fluid className="py-4 my-4 border border-2 rounded-3">
       <div className="mb-4 text-center">
-        <h1 className="">Add a new Book Notes</h1>
+        <h1 className="">Add a new book notes</h1>
       </div>
 
-      <Form>
+      <Form onSubmit={handleSubmit(addBook)}>
         <Form.Group as={Row} className="mb-3" controlId="formTitle">
           <Form.Label column sm={2}>
             Title
           </Form.Label>
           <Col sm={10}>
-            <Form.Control type="text" placeholder="Book Title" />
+            <Form.Control
+              type="text"
+              placeholder="Book Title"
+              {...(register('title'), { required: true })}
+            />
           </Col>
         </Form.Group>
         <Form.Group as={Row} className="mb-3" controlId="formDescription">
@@ -101,7 +122,11 @@ function BookForm(props: BookFormProps) {
             Description
           </Form.Label>
           <Col sm={10}>
-            <Form.Control as="textarea" rows={2} />
+            <Form.Control
+              as="textarea"
+              rows={2}
+              {...(register('description'), { required: true })}
+            />
           </Col>
         </Form.Group>
         <Form.Group as={Row} className="mb-3" controlId="formStartDate">
@@ -109,7 +134,10 @@ function BookForm(props: BookFormProps) {
             Start date
           </Form.Label>
           <Col sm={10}>
-            <Form.Control type="date" />
+            <Form.Control
+              type="date"
+              {...(register('start_date'), { required: true })}
+            />
           </Col>
         </Form.Group>
         <Form.Group as={Row} className="mb-3" controlId="formEndDate">
@@ -117,23 +145,31 @@ function BookForm(props: BookFormProps) {
             End date
           </Form.Label>
           <Col sm={10}>
-            <Form.Control type="date" />
+            <Form.Control type="date" {...register('end_date')} />
           </Col>
         </Form.Group>
-        <Form.Group as={Row} className="mb-3" controlId="formEndDate">
+        <Form.Group as={Row} className="mb-3" controlId="formImageUrl">
           <Form.Label column sm={2}>
             Image url
           </Form.Label>
           <Col sm={10}>
-            <Form.Control type="url" placeholder="https://example.com" />
+            <Form.Control
+              type="url"
+              placeholder="https://example.com"
+              {...register('image_url')}
+            />
           </Col>
         </Form.Group>
-        <Form.Group as={Row} className="mb-3" controlId="formEndDate">
+        <Form.Group as={Row} className="mb-3" controlId="formAmazonProductUrl">
           <Form.Label column sm={2}>
             Amazon product url
           </Form.Label>
           <Col sm={10}>
-            <Form.Control type="url" placeholder="https://example.com" />
+            <Form.Control
+              type="url"
+              placeholder="https://example.com"
+              {...register('amazon_product_url')}
+            />
           </Col>
         </Form.Group>
 
@@ -143,7 +179,7 @@ function BookForm(props: BookFormProps) {
             One line Summary
           </Form.Label>
           <Col sm={10}>
-            <Form.Control type="text" />
+            <Form.Control type="text" {...register('summary.oneLiner')} />
           </Col>
         </Form.Group>
         <Form.Group as={Row} className="mb-3" controlId="threeLinerSummary">
@@ -151,7 +187,11 @@ function BookForm(props: BookFormProps) {
             Three line Summary
           </Form.Label>
           <Col sm={10}>
-            <Form.Control as="textarea" rows={3} />
+            <Form.Control
+              as="textarea"
+              rows={3}
+              {...register('summary.threeLiner')}
+            />
           </Col>
         </Form.Group>
         <Form.Group as={Row} className="mb-3" controlId="detailedSummary">
@@ -159,13 +199,17 @@ function BookForm(props: BookFormProps) {
             Detailed Summary
           </Form.Label>
           <Col sm={10}>
-            <Form.Control as="textarea" rows={5} />
+            <Form.Control
+              as="textarea"
+              rows={5}
+              {...register('summary.detailed')}
+            />
           </Col>
         </Form.Group>
 
         <h3 className="mb-3">Book Hightlights</h3>
-        {highlightRows.map((_highlightRow, index) => (
-          <HighlightRow id={index.toString()} />
+        {fields.map((field, index) => (
+          <HighlightRow field={field} index={index} />
         ))}
 
         <Row>
@@ -173,9 +217,7 @@ function BookForm(props: BookFormProps) {
           <Col sm={10}>
             <Button
               className="mb-5"
-              onClick={() =>
-                setHighlightRows(highlightRows.concat(defaultHighlightRow))
-              }
+              onClick={() => append(defaultHighlightRow, { shouldFocus: true })}
             >
               Add new Hightlight
             </Button>
@@ -188,7 +230,7 @@ function BookForm(props: BookFormProps) {
           controlId="formSubmit"
         >
           <Col className="col-6 align-self-center">
-            <Button className="w-100" onClick={addBook}>
+            <Button className="w-100" type="submit">
               Submit
             </Button>
           </Col>
